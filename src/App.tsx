efa2ce6d65f92e6e8fb0964f001540922d2e775f
ibdkn <ibudiakina@gmail.com>
2024-04-01 13:56:01 +0300

    added layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
 import React from 'react';
 import './App.css';
+import logo from "./assets/images/logo.png"
+import gate from "./assets/images/gate.jpeg"
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img src={logo} alt=""/>
+            </header>
+            <nav className="nav">
+                <ul>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Settings</a></li>
+                </ul>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src={gate} alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
-const Header = () => {
-    return (
-        <div>
-            <a href="#">Home</a>
-            <a href="#">Hews Feed</a>
-            <a href="#">Messages</a>
-        </div>
-    );
-}
-
-const Technologies = () => {
-    return (
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    );
-}
 
 export default App;
