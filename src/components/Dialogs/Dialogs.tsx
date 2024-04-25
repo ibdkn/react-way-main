d4f8ce39ba1d464e25f2d91ba65e4af3ade7c475
ibdkn <ibudiakina@gmail.com>
2024-04-25 03:53:09 +0300

    added dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import s from "./Dialogs.module.css"
+import s from "./Dialog.module.css"
 
 export const Dialogs = () => {
     return (
-        <div>
-           Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valery
+                </div>
+            </div>
+            <div className={s.messagesItems}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+            </div>
         </div>
     );
 };
